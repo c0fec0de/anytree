11c45dbd0bc1766096a273a4331891ad5efd5f8e
c0fec0de <c0fec0de@gmail.com>
2017-03-15 10:43:56 +0100

    Extend test.

---
+++ tests/test_examples.py
@@ -0,0 +1,29 @@
+from nose.tools import eq_
+
+from anytree import Node, RenderTree
+
+
+def test_stackoverflow():
+    """Example from stackoverflow."""
+    udo = Node("Udo")
+    marc = Node("Marc", parent=udo)
+    Node("Lian", parent=marc)
+    dan = Node("Dan", parent=udo)
+    Node("Jet", parent=dan)
+    Node("Jan", parent=dan)
+    joe = Node("Joe", parent=dan)
+
+    eq_(str(udo), "Node('/Udo')")
+    eq_(str(joe), "Node('/Udo/Dan/Joe')")
+
+    eq_(["%s%s" % (pre, node.name) for pre, fill, node in RenderTree(udo)], [
+        u"Udo",
+        u"├── Marc",
+        u"│   └── Lian",
+        u"└── Dan",
+        u"    ├── Jet",
+        u"    ├── Jan",
+        u"    └── Joe",
+    ])
+    eq_(str(dan.children),
+        "(Node('/Udo/Dan/Jet'), Node('/Udo/Dan/Jan'), Node('/Udo/Dan/Joe'))")
