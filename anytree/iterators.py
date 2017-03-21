ee0e5a7c816505063d97b58431e8c8e0811abf2a
c0fec0de <c0fec0de@gmail.com>
2017-03-21 17:37:53 +0100

    Implement LevelOrderIter.

--- anytree/iterators.py
+++ anytree/iterators.py
@@ -93,3 +93,48 @@ class PostOrderIter(object):
             for grandchild in cls.__next(child.children):
                 yield grandchild
             yield child
+
+
+class LevelOrderIter(object):
+
+    def __init__(self, node):
+        """
+        Iterate over tree applying level-order strategy starting at `node`.
+
+        >>> from anytree import Node, RenderTree, AsciiStyle
+        >>> f = Node("f")
+        >>> b = Node("b", parent=f)
+        >>> a = Node("a", parent=b)
+        >>> d = Node("d", parent=b)
+        >>> c = Node("c", parent=d)
+        >>> e = Node("e", parent=d)
+        >>> g = Node("g", parent=f)
+        >>> i = Node("i", parent=g)
+        >>> h = Node("h", parent=i)
+        >>> print(RenderTree(f, style=AsciiStyle()))
+        Node('/f')
+        |-- Node('/f/b')
+        |   |-- Node('/f/b/a')
+        |   +-- Node('/f/b/d')
+        |       |-- Node('/f/b/d/c')
+        |       +-- Node('/f/b/d/e')
+        +-- Node('/f/g')
+            +-- Node('/f/g/i')
+                +-- Node('/f/g/i/h')
+
+        >>> [node.name for node in LevelOrderIter(f)]
+        ['f', 'b', 'g', 'a', 'd', 'i', 'c', 'e', 'h']
+        """
+        super(LevelOrderIter, self).__init__()
+        self.node = node
+
+    def __iter__(self):
+        node = self.node
+        yield node
+        children = node.children
+        while children:
+            next_children = []
+            for child in children:
+                yield child
+                next_children += child.children
+            children = next_children
---
+++ tests/test_iterators.py
@@ -0,0 +1,18 @@
+from anytree import LevelOrderIter
+from anytree import Node
+from nose.tools import eq_
+
+
+def test_levelorder():
+    """LevelOrderIter."""
+    f = Node("f")
+    b = Node("b", parent=f)
+    a = Node("a", parent=b)
+    d = Node("d", parent=b)
+    c = Node("c", parent=d)
+    e = Node("e", parent=d)
+    g = Node("g", parent=f)
+    i = Node("i", parent=g)
+    h = Node("h", parent=i)
+
+    eq_(list(LevelOrderIter(f)), [f, b, g, a, d, i, c, e, h])
