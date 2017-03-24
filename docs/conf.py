b668ec6ad933b0e1a56a2139ca47b41dc93773be
c0fec0de <c0fec0de@gmail.com>
2017-03-24 22:40:33 +0100

    Add google-stype doc.

--- docs/conf.py
+++ docs/conf.py
@@ -42,6 +42,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.doctest',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
