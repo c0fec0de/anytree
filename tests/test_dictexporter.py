a20837eee5397ee959e0507caf2191d9a1cd7f24
c0fec0de <c0fec0de@gmail.com>
2017-11-14 21:42:49 +0100

    Add tests.

---
+++ tests/test_dictexporter.py
@@ -0,0 +1,35 @@
+from nose.tools import eq_
+
+from anytree import AnyNode
+from anytree.exporter import DictExporter
+from anytree import RenderTree
+
+
+def test_dict_exporter():
+    """Dict Exporter."""
+    root = AnyNode(id="root")
+    s0 = AnyNode(id="sub0", parent=root)
+    s0b = AnyNode(id="sub0B", parent=s0)
+    s0a = AnyNode(id="sub0A", parent=s0)
+    s1 = AnyNode(id="sub1", parent=root)
+    s1a = AnyNode(id="sub1A", parent=s1)
+    s1b = AnyNode(id="sub1B", parent=s1)
+    s1c = AnyNode(id="sub1C", parent=s1)
+    s1ca = AnyNode(id="sub1Ca", parent=s1c)
+
+    exporter = DictExporter()
+    eq_(exporter.export(root),
+        {'id': 'root', 'children': [
+            {'id': 'sub0', 'children': [
+                {'id': 'sub0B'},
+                {'id': 'sub0A'}
+            ]},
+            {'id': 'sub1', 'children': [
+                {'id': 'sub1A'},
+                {'id': 'sub1B'},
+                {'id': 'sub1C', 'children': [
+                    {'id': 'sub1Ca'}
+                ]}
+            ]}
+        ]}
+    )
