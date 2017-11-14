a01712fa15bd0aaed311cd5930dbb8807116e697
c0fec0de <c0fec0de@gmail.com>
2017-11-14 21:06:57 +0100

    Fix doc.

--- anytree/node.py
+++ anytree/node.py
@@ -3,7 +3,7 @@
 Node Classes.
 
 * :any:`Node`: a simple tree node with at least a name attribute and any number of additional attributes.
-* :any:`AnyNode`: a generic tree node and any number of additional attributes.
+* :any:`AnyNode`: a generic tree node with any number of attributes.
 * :any:`NodeMixin`: extends any python class to a tree node.
 """
 
