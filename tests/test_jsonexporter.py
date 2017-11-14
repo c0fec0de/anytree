9596bf259ae398f9152244dd6db334a04366b521
c0fec0de <c0fec0de@gmail.com>
2017-11-14 22:28:04 +0100

    Also test write function.

--- tests/test_jsonexporter.py
+++ tests/test_jsonexporter.py
@@ -1,11 +1,16 @@
+import filecmp
+import os
+
+from tempfile import TemporaryDirectory
+
 from nose.tools import eq_
 
 from anytree import AnyNode
 from anytree.exporter import JsonExporter
 
 
-def test_dict_exporter():
-    """Dict Exporter."""
+def test_json_exporter():
+    """Json Exporter."""
     root = AnyNode(id="root")
     s0 = AnyNode(id="sub0", parent=root)
     s0b = AnyNode(id="sub0B", parent=s0)
@@ -16,41 +21,51 @@ def test_dict_exporter():
     s1c = AnyNode(id="sub1C", parent=s1)
     s1ca = AnyNode(id="sub1Ca", parent=s1c)
 
-    exporter = JsonExporter(indent=2, sort_keys=True)
-    eq_(exporter.export(root), (
-        '{\n'
-        '  "children": [\n'
-        '    {\n'
-        '      "children": [\n'
-        '        {\n'
-        '          "id": "sub0B"\n'
-        '        },\n'
-        '        {\n'
-        '          "id": "sub0A"\n'
-        '        }\n'
-        '      ],\n'
-        '      "id": "sub0"\n'
-        '    },\n'
-        '    {\n'
-        '      "children": [\n'
-        '        {\n'
-        '          "id": "sub1A"\n'
-        '        },\n'
-        '        {\n'
-        '          "id": "sub1B"\n'
-        '        },\n'
-        '        {\n'
-        '          "children": [\n'
-        '            {\n'
-        '              "id": "sub1Ca"\n'
-        '            }\n'
-        '          ],\n'
-        '          "id": "sub1C"\n'
-        '        }\n'
-        '      ],\n'
-        '      "id": "sub1"\n'
-        '    }\n'
-        '  ],\n'
-        '  "id": "root"\n'
+    lines = [
+        '{',
+        '  "children": [',
+        '    {',
+        '      "children": [',
+        '        {',
+        '          "id": "sub0B"',
+        '        },',
+        '        {',
+        '          "id": "sub0A"',
+        '        }',
+        '      ],',
+        '      "id": "sub0"',
+        '    },',
+        '    {',
+        '      "children": [',
+        '        {',
+        '          "id": "sub1A"',
+        '        },',
+        '        {',
+        '          "id": "sub1B"',
+        '        },',
+        '        {',
+        '          "children": [',
+        '            {',
+        '              "id": "sub1Ca"',
+        '            }',
+        '          ],',
+        '          "id": "sub1C"',
+        '        }',
+        '      ],',
+        '      "id": "sub1"',
+        '    }',
+        '  ],',
+        '  "id": "root"',
         '}'
-    ))
+    ]
+
+    exporter = JsonExporter(indent=2, sort_keys=True)
+    eq_(exporter.export(root), "\n".join(lines))
+    with TemporaryDirectory() as tmpdirname:
+        genfile = tmpdirname + os.sep + "gen.txt"
+        reffile = tmpdirname + os.sep + "ref.txt"
+        with open(genfile, "w") as gen:
+            exporter.write(root, gen)
+            with open(reffile, "w") as ref:
+                ref.write("\n".join(lines))
+        assert filecmp.cmp(reffile, genfile)
