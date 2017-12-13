1d67b7826238bb729791308fba2ad408949cd534
c0fec0de <c0fec0de@gmail.com>
2017-12-13 18:21:29 +0100

    Extend testing.

--- tests/test_dictexporter.py
+++ tests/test_dictexporter.py
@@ -1,6 +1,7 @@
 from nose.tools import eq_
 
 from anytree import AnyNode
+from anytree import Node
 from anytree.exporter import DictExporter
 
 
@@ -10,7 +11,7 @@ def test_dict_exporter():
     s0 = AnyNode(id="sub0", parent=root)
     s0b = AnyNode(id="sub0B", parent=s0)
     s0a = AnyNode(id="sub0A", parent=s0)
-    s1 = AnyNode(id="sub1", parent=root)
+    s1 = AnyNode(id="sub1", parent=root, foo="bar")
     s1a = AnyNode(id="sub1A", parent=s1)
     s1b = AnyNode(id="sub1B", parent=s1)
     s1c = AnyNode(id="sub1C", parent=s1)
@@ -23,11 +24,69 @@ def test_dict_exporter():
                 {'id': 'sub0B'},
                 {'id': 'sub0A'}
             ]},
-            {'id': 'sub1', 'children': [
+            {'id': 'sub1', 'foo':'bar', 'children': [
                 {'id': 'sub1A'},
                 {'id': 'sub1B'},
                 {'id': 'sub1C', 'children': [
                     {'id': 'sub1Ca'}
+                ]}
+            ]}
+        ]}
+    )
+
+def test_dict_exporter_node():
+    """Dict Exporter."""
+    root = Node("root")
+    s0 = Node("sub0", parent=root)
+    s0b = Node("sub0B", parent=s0)
+    s0a = Node("sub0A", parent=s0)
+    s1 = Node("sub1", parent=root, foo="bar")
+    s1a = Node("sub1A", parent=s1)
+    s1b = Node("sub1B", parent=s1)
+    s1c = Node("sub1C", parent=s1)
+    s1ca = Node("sub1Ca", parent=s1c)
+
+    exporter = DictExporter()
+    eq_(exporter.export(root),
+        {'name': 'root', 'children': [
+            {'name': 'sub0', 'children': [
+                {'name': 'sub0B'},
+                {'name': 'sub0A'}
+            ]},
+            {'name': 'sub1', 'foo':'bar', 'children': [
+                {'name': 'sub1A'},
+                {'name': 'sub1B'},
+                {'name': 'sub1C', 'children': [
+                    {'name': 'sub1Ca'}
+                ]}
+            ]}
+        ]}
+    )
+
+def test_dict_exporter_filter():
+    """Dict Exporter."""
+    root = Node("root")
+    s0 = Node("sub0", parent=root)
+    s0b = Node("sub0B", parent=s0)
+    s0a = Node("sub0A", parent=s0)
+    s1 = Node("sub1", parent=root, foo="bar")
+    s1a = Node("sub1A", parent=s1)
+    s1b = Node("sub1B", parent=s1)
+    s1c = Node("sub1C", parent=s1)
+    s1ca = Node("sub1Ca", parent=s1c)
+
+    exporter = DictExporter(attriter=lambda attrs: [(k, v) for k, v in attrs if k == "name"])
+    eq_(exporter.export(root),
+        {'name': 'root', 'children': [
+            {'name': 'sub0', 'children': [
+                {'name': 'sub0B'},
+                {'name': 'sub0A'}
+            ]},
+            {'name': 'sub1', 'children': [
+                {'name': 'sub1A'},
+                {'name': 'sub1B'},
+                {'name': 'sub1C', 'children': [
+                    {'name': 'sub1Ca'}
                 ]}
             ]}
         ]}
--- tests/test_dictimporter.py
+++ tests/test_dictimporter.py
@@ -1,10 +1,12 @@
 from copy import deepcopy
 from nose.tools import eq_
 
+from anytree import Node
+from anytree import RenderTree
 from anytree.exporter import DictExporter
 from anytree.importer import DictImporter
 
-
+from helper import eq_str
 
 def test_dict_importer():
     """Dict Importer."""
@@ -28,4 +30,53 @@ def test_dict_importer():
     root = importer.import_(data)
     eq_(data, refdata)
     eq_(exporter.export(root), data)
+    r = RenderTree(root)
+    expected = u"\n".join([
+        u"AnyNode(id='root')",
+        u"├── AnyNode(id='sub0')",
+        u"│   ├── AnyNode(id='sub0B')",
+        u"│   └── AnyNode(id='sub0A')",
+        u"└── AnyNode(id='sub1')",
+        u"    ├── AnyNode(id='sub1A')",
+        u"    ├── AnyNode(id='sub1B')",
+        u"    └── AnyNode(id='sub1C')",
+        u"        └── AnyNode(id='sub1Ca')",
+    ])
+    eq_str(str(r), expected)
 
+
+def test_dict_importer_node():
+    """Dict Importer."""
+    importer = DictImporter(Node)
+    exporter = DictExporter()
+    refdata = {
+        'name': 'root', 'children': [
+            {'name': 'sub0', 'children': [
+                {'name': 'sub0B'},
+                {'name': 'sub0A'}
+            ]},
+            {'name': 'sub1', 'children': [
+                {'name': 'sub1A'},
+                {'name': 'sub1B'},
+                {'name': 'sub1C', 'children': [
+                    {'name': 'sub1Ca'}
+                ]}
+            ]}
+        ]}
+    data = deepcopy(refdata)
+    root = importer.import_(data)
+    eq_(data, refdata)
+    eq_(exporter.export(root), data)
+    r = RenderTree(root)
+    expected = u"\n".join([
+        u"Node('/root')",
+        u"├── Node('/root/sub0')",
+        u"│   ├── Node('/root/sub0/sub0B')",
+        u"│   └── Node('/root/sub0/sub0A')",
+        u"└── Node('/root/sub1')",
+        u"    ├── Node('/root/sub1/sub1A')",
+        u"    ├── Node('/root/sub1/sub1B')",
+        u"    └── Node('/root/sub1/sub1C')",
+        u"        └── Node('/root/sub1/sub1C/sub1Ca')",
+    ])
+    eq_str(str(r), expected)
