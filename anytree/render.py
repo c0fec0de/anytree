af3a7514bae6084ae286e644031a2be7dbe4fce1
c0fec0de <c0fec0de@gmail.com>
2019-09-24 23:33:53 +0200

    Clarify use of get_attr, fixes #91.

--- anytree/render.py
+++ anytree/render.py
@@ -300,7 +300,32 @@ class RenderTree(object):
         return "%s(%s)" % (classname, ", ".join(args))
 
     def by_attr(self, attrname="name"):
-        """Return rendered tree with node attribute `attrname`."""
+        """
+        Return rendered tree with node attribute `attrname`.
+
+        >>> from anytree import AnyNode, RenderTree
+        >>> root = AnyNode(id="root")
+        >>> s0 = AnyNode(id="sub0", parent=root)
+        >>> s0b = AnyNode(id="sub0B", parent=s0, foo=4, bar=109)
+        >>> s0a = AnyNode(id="sub0A", parent=s0)
+        >>> s1 = AnyNode(id="sub1", parent=root)
+        >>> s1a = AnyNode(id="sub1A", parent=s1)
+        >>> s1b = AnyNode(id="sub1B", parent=s1, bar=8)
+        >>> s1c = AnyNode(id="sub1C", parent=s1)
+        >>> s1ca = AnyNode(id="sub1Ca", parent=s1c)
+        >>> print(RenderTree(root).by_attr('id'))
+        root
+        ├── sub0
+        │   ├── sub0B
+        │   └── sub0A
+        └── sub1
+            ├── sub1A
+            ├── sub1B
+            └── sub1C
+                └── sub1Ca
+
+
+        """
         def get():
             for pre, fill, node in self:
                 attr = attrname(node) if callable(attrname) else getattr(node, attrname, "")
