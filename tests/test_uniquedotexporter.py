aab1dc0eac86230f8cfeb15f0644c2664b780637
paul <37733348+eckp@users.noreply.github.com>
2022-03-11 13:41:10 +0100

    Made UniqueDotExporter output deterministic
    Changed from python object ids to a simple counter as DOT node name.
    This way identical trees will produce identical DOT output, regardless of their location in memory.
    Doctests now work too, because the output does not vary anymore.

--- tests/test_uniquedotexporter.py
+++ tests/test_uniquedotexporter.py
@@ -19,15 +19,11 @@ def test_tree1():
     s0 = Node("sub0", parent=root)
     s0b = Node("sub0B", parent=s0)
 
-    id_root = hex(id(root))
-    id_s0 = hex(id(s0))
-    id_s0b = hex(id(s0b))
-
     lines = tuple(UniqueDotExporter(root))
     eq_(lines, ('digraph tree {',
-                '    "{id_root}" [label="root"];'.format(id_root=id_root),
-                '    "{id_s0}" [label="sub0"];'.format(id_s0=id_s0),
-                '    "{id_s0b}" [label="sub0B"];'.format(id_s0b=id_s0b),
-                '    "{id_root}" -> "{id_s0}";'.format(id_root=id_root, id_s0=id_s0),
-                '    "{id_s0}" -> "{id_s0b}";'.format(id_s0=id_s0, id_s0b=id_s0b),
+                '    "0" [label="root"];',
+                '    "1" [label="sub0"];',
+                '    "2" [label="sub0B"];',
+                '    "0" -> "1";',
+                '    "1" -> "2";',
                 '}'))
