814da5904dacd1ba564be7188e0d9508ec95f67b
c0fec0de <c0fec0de@gmail.com>
2023-06-25 22:17:16 +0200

    219 end python 36 testing add 311 (#220)
    * end 3.6, add 3.11

--- anytree/dotexport.py
+++ anytree/dotexport.py
@@ -7,6 +7,6 @@ class RenderTreeGraph(DotExporter):
     def __init__(self, *args, **kwargs):
         """Legacy. Use :any:`anytree.exporter.DotExporter` instead."""
         warnings.warn(
-            ("anytree.RenderTreeGraph has moved. " "Use anytree.exporter.DotExporter instead"), DeprecationWarning
+            ("anytree.RenderTreeGraph has moved. Use anytree.exporter.DotExporter instead"), DeprecationWarning
         )
         super(RenderTreeGraph, self).__init__(*args, **kwargs)
