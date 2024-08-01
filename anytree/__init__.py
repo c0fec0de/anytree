a182903742ddc8d77cf8e01c1957c011dc65320e
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2024-08-01 00:39:40 -0500

    Tell typecheckers we are explicity exporting objects

--- anytree/__init__.py
+++ anytree/__init__.py
@@ -8,38 +8,38 @@ __author_email__ = "c0fec0de@gmail.com"
 __description__ = """Powerful and Lightweight Python Tree Data Structure."""
 __url__ = "https://github.com/c0fec0de/anytree"
 
-from . import cachedsearch  # noqa
-from . import util  # noqa
-from .iterators import LevelOrderGroupIter  # noqa
-from .iterators import LevelOrderIter  # noqa
-from .iterators import PostOrderIter  # noqa
-from .iterators import PreOrderIter  # noqa
-from .iterators import ZigZagGroupIter  # noqa
-from .node import AnyNode  # noqa
-from .node import LightNodeMixin  # noqa
-from .node import LoopError  # noqa
-from .node import Node  # noqa
-from .node import NodeMixin  # noqa
-from .node import SymlinkNode  # noqa
-from .node import SymlinkNodeMixin  # noqa
-from .node import TreeError  # noqa
-from .render import AbstractStyle  # noqa
-from .render import AsciiStyle  # noqa
-from .render import ContRoundStyle  # noqa
-from .render import ContStyle  # noqa
-from .render import DoubleStyle  # noqa
-from .render import RenderTree  # noqa
-from .resolver import ChildResolverError  # noqa
-from .resolver import Resolver  # noqa
-from .resolver import ResolverError  # noqa
-from .resolver import RootResolverError  # noqa
-from .search import CountError  # noqa
-from .search import find  # noqa
-from .search import find_by_attr  # noqa
-from .search import findall  # noqa
-from .search import findall_by_attr  # noqa
-from .walker import Walker  # noqa
-from .walker import WalkError  # noqa
+from . import cachedsearch as cachedsearch  # noqa
+from . import util as util  # noqa
+from .iterators import LevelOrderGroupIter as LevelOrderGroupIter  # noqa
+from .iterators import LevelOrderIter as LevelOrderIter  # noqa
+from .iterators import PostOrderIter as PostOrderIter  # noqa
+from .iterators import PreOrderIter as PreOrderIter  # noqa
+from .iterators import ZigZagGroupIter as ZigZagGroupIter  # noqa
+from .node import AnyNode as AnyNode  # noqa
+from .node import LightNodeMixin as LightNodeMixin  # noqa
+from .node import LoopError as LoopError  # noqa
+from .node import Node as Node  # noqa
+from .node import NodeMixin as NodeMixin  # noqa
+from .node import SymlinkNode as SymlinkNode  # noqa
+from .node import SymlinkNodeMixin as SymlinkNodeMixin  # noqa
+from .node import TreeError as TreeError  # noqa
+from .render import AbstractStyle as AbstractStyle  # noqa
+from .render import AsciiStyle as AsciiStyle  # noqa
+from .render import ContRoundStyle as ContRoundStyle  # noqa
+from .render import ContStyle as ContStyle  # noqa
+from .render import DoubleStyle as DoubleStyle  # noqa
+from .render import RenderTree as RenderTree  # noqa
+from .resolver import ChildResolverError as ChildResolverError  # noqa
+from .resolver import Resolver as Resolver  # noqa
+from .resolver import ResolverError as ResolverError  # noqa
+from .resolver import RootResolverError as RootResolverError  # noqa
+from .search import CountError as CountError  # noqa
+from .search import find as find  # noqa
+from .search import find_by_attr as find_by_attr  # noqa
+from .search import findall as findall  # noqa
+from .search import findall_by_attr as findall_by_attr  # noqa
+from .walker import Walker as Walker  # noqa
+from .walker import WalkError as WalkError  # noqa
 
 # legacy
 LevelGroupOrderIter = LevelOrderGroupIter
